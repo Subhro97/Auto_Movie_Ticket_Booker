03da303e017dfae4fdadfa31ffe0a75de32b1c8b
Subhrojit Mondal <subhromond@gmail.com>
2021-04-18 19:00:02 +0530

    Hackathon

--- Readme.js
+++ Readme.js
@@ -1,11 +1,11 @@
 /*
 Project: Automated Movie Ticket Booker.
 
-Description: In this project, I have created an Automated Movie Ticket Booker. The software takes input of the movie details such as name,
+Description: In this project, I have created an Automated Movie Ticket Booker software. The software takes as input the movie details such as name,
  tickets, date etc. and goes to the Bookmyshow website, and books the ticket for the user. It also fetches back the ticket details in json and
  PDF format as well as a screenshot of the ticket details.
 
- Files:
+ Files/Folders:
  1. movieBooker.js: This file contains the code for going to the website and booking the tickets.
  2. input.json: This file contains the details of the movie as entered by the user.
  3. Movies_Details: This file contains the ticket details fetched during booking.
